08a2134549f4f4cd3489549443c54a7dcfd7bd06
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-19 21:47:41 +0530

    Commit new files...

---
+++ Python_Core/com/core/Add_two_umbers.py
@@ -0,0 +1,8 @@
+# This program adds two numbers
+
+num1 = 2.5
+num2 = 3.4
+
+sum = num1 + num2
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
---
+++ Python_Core/com/core/Add_two_umbers_with_input.py
@@ -0,0 +1,13 @@
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+sum = float(num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Test data:
+# Enter first number: 1.1
+# Enter second number: 2.2
+# The sum of 1.1 and 2.2 is 3.3000000000000003
+
