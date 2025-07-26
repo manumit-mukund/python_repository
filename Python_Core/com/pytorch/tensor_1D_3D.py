6776e12326b015928761092325385a35d82ef6cf
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-07-26 10:02:41 +0530

    Commit changes...

--- Python_Core/com/pytorch/tensor_1D_3D.py
+++ Python_Core/com/pytorch/tensor_1D_3D.py
@@ -1,9 +1,25 @@
 import torch
 
-# Creating a 1D tensor
-x = torch.tensor([1.0, 2.0, 3.0])
-print('1D Tensor: \n', x)
+tensor_1d = torch.tensor([1, 2, 3])
+print("1D Tensor (Vector):")
+print(tensor_1d)
+print()
 
-# Creating a 2D tensor
-y = torch.zeros((3, 3))
-print('2D Tensor: \n', y)
+tensor_2d = torch.tensor([[1, 2], [3, 4]])
+print("2D Tensor (Matrix):")
+print(tensor_2d)
+print()
+
+random_tensor = torch.rand(2, 3)
+print("Random Tensor (2x3):")
+print(random_tensor)
+print()
+
+zeros_tensor = torch.zeros(2, 3)
+print("Zeros Tensor (2x3):")
+print(zeros_tensor)
+print()
+
+ones_tensor = torch.ones(2, 3)
+print("Ones Tensor (2x3):")
+print(ones_tensor)
