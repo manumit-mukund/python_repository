3c95606af58a4f0a6fe8b45d9a5b565b27b66c8f
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-07-26 10:09:07 +0530

    Commit changes...

--- Python_Core/com/pytorch/tensor_matrix_add_mul.py
+++ Python_Core/com/pytorch/tensor_matrix_add_mul.py
@@ -9,3 +9,13 @@ print('Element Wise Addition of a & b: \n', a + b)
 # Matrix multiplication
 print('Matrix Multiplication of a & b: \n',
       torch.matmul(a.view(2, 1), b.view(1, 2)))
+
+tensor_a = torch.tensor([[1, 2, 3], [4, 5, 6]])
+
+tensor_b = torch.tensor([[10, 20, 30]])
+
+broadcasted_result = tensor_a + tensor_b
+print(f"Broadcasted Addition Result: \n{broadcasted_result}")
+
+matrix_multiplication_result = torch.matmul(tensor_a, tensor_a.T)
+print(f"Matrix Multiplication Result (tensor_a * tensor_a^T): \n{matrix_multiplication_result}")
