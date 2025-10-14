caa99609682b1bcb597e977bfe56d52efcd81f02
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-10-14 12:41:35 +0530

    Commit changes...

--- Python_Core/com/numpy/Numpy_array_slice.py
+++ Python_Core/com/numpy/Numpy_array_slice.py
@@ -5,7 +5,7 @@ arr1 = np.array([1, 2, 3, 4, 5])
 print("1D Array:", arr1)
 
 # Creating a 2D array
-arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+arr2 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 print("\n2D Array:\n", arr2)
 
 # Slicing arrays
